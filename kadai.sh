47037f77906095b4c97c480eb71b63d68664ce5c
Ayu UCHIKAWA <a2004au@aiit.ac.jp>
2020-06-23 22:12:08 +0900

    Create kadai.sh
    - First commit

---
+++ kadai.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+read -p "enter first number: " num1
+read -p "enter second number: " num2
+
+echo "You entered $num1 and $num2"
+
+if [ ${num1} -lt 1 ] || [ ${num2} -lt 1 ]; then
+echo "You entered wrong number"
+exit 1
+
+fi
+
+expr ${num1} + 1 > /dev/null 2>&1
+if [ $? -lt 2 ]; then
+echo "OK"
+else
+echo "Error occured. Please try again."
+exit 1
+fi
+
+
+function gcd() {
+	if (( $1 % $2 == 0 )); then
+		echo $2
+	else
+		gcd $2 $(( $1 % $2 ))
+	fi
+}
+
+echo $(gcd ${num1} ${num2})
+  
